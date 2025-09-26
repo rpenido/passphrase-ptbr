0de48695034a6c3bdeab6b4f125f39d7f156bce0
Rômulo Penido <romulo.penido@gmail.com>
2025-09-26 10:57:42 -0300

    fix: ptbr strings

--- passphrase.py
+++ passphrase.py
@@ -6,16 +6,16 @@ if __name__ == '__main__':
     lines = file.readlines()
     num_lines = len(lines)
     passphrases = []
-    input_words = input('How many words do you want in your passphrase? (5): ')
+    input_words = input('Quantas palavras você quer em sua senha? (5): ')
     passphrase_words = int(input_words)
     while len(passphrases) < passphrase_words:
         random_line = randrange(num_lines)
         word = lines[random_line].strip()
-        print(f'Word: {word}')
-        input_continue = input('Do you want to add this word to your passphrase? (y/N): ')
+        print(f'Palavra: {word}')
+        input_continue = input('Você quer adicionar esta palavra à sua senha? (y/N): ')
         if input_continue == 'y':
             passphrases.append(word)
         else:
             continue
 
-    print(f'Passphrase: {" ".join(passphrases)}')
+    print(f'Senha: {" ".join(passphrases)}')
